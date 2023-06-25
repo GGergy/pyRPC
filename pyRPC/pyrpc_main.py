f639e21f9939ede88a61cb33152f973199cc3934
Gergy <gergy2k07@gmail.com>
2023-06-25 18:05:59 +0300

    add deleting, local cfg

--- pyRPC/pyrpc_main.py
+++ pyRPC/pyrpc_main.py
@@ -1,10 +1,16 @@
-import os.path
+import os
+
+import telebot.apihelper
+
+if __name__ == "__main__":
+    os.chdir(os.path.split(os.getcwd())[0])
+    os.system("python test.py")
+    quit()
 from io import BytesIO
 from .util import tools
 from telebot import types
 
-
-tools.init(__file__)
+tools.init()
 bot = tools.load_bot()
 
 
@@ -21,8 +27,9 @@ def start(message):
     tools.create_host(message.chat.id)
     if not access(message):
         return
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('/explorer'), types.KeyboardButton('/processes'), row_width=2)
+    markup = types.InlineKeyboardMarkup()
+    markup.row(types.InlineKeyboardButton('explorer', callback_data="explore::.::1"))
+    markup.row(types.InlineKeyboardButton('process manager', callback_data='process'))
     bot.send_message(chat_id=message.chat.id, text='host connected successfully', reply_markup=markup)
 
 
@@ -55,14 +62,13 @@ def explorer_func(path):
         buttons += 1
     btns = []
     if path != '.':
-        print(path)
         btns.append(types.InlineKeyboardButton(text='🔙', callback_data=f"explore::{os.path.dirname(path)}"))
         if len(path.split('\\')) > 2:
             btns.append(types.InlineKeyboardButton(text='🏠', callback_data=f"explore::."))
-    btns.append(types.InlineKeyboardButton(text='❌', callback_data='close'))
     markup.row(*btns)
     markup.row(types.InlineKeyboardButton(text='🔄', callback_data=f"explore::{path}"),
                types.InlineKeyboardButton(text='📤upload', callback_data=f"upload::{path}"))
+    markup.row(types.InlineKeyboardButton(text='❌', callback_data='close'))
     return markup
 
 
@@ -71,15 +77,20 @@ def handle_explorer(call):
     if not access(call.message):
         return
     path = call.data.split("::")[1]
+    new = len(call.data.split("::")) == 3
     msg = explorer_func(path)
     if not msg:
         bot.answer_callback_query(callback_query_id=call.id, text='its empty folder')
         return
     try:
-        bot.edit_message_text(text='your files', reply_markup=msg, chat_id=call.message.chat.id,
-                              message_id=call.message.id)
-    except:
-        bot.answer_callback_query(callback_query_id=call.id, text='there are no updates')
+        if new:
+            bot.send_message(text='your files', reply_markup=msg, chat_id=call.message.chat.id)
+            bot.answer_callback_query(callback_query_id=call.id)
+        else:
+            bot.edit_message_text(text='your files', reply_markup=msg, chat_id=call.message.chat.id,
+                                  message_id=call.message.id)
+    except telebot.apihelper.ApiTelegramException:
+        bot.answer_callback_query(callback_query_id=call.id, text='there are not updates')
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('selectfile'))
@@ -88,7 +99,8 @@ def file_view(call):
     notpath = filename[filename.rfind('\\') + 1:]
     markup = types.InlineKeyboardMarkup()
     markup.row(types.InlineKeyboardButton(text='⬇️download', callback_data=f"download::{filename}"))
-    markup.row(types.InlineKeyboardButton(text='🗑️delete', callback_data=f"delete::{filename}"))
+    markup.row(types.InlineKeyboardButton(text='🗑️delete', callback_data=f"&delete::{filename}"))
+    markup.row(types.InlineKeyboardButton(text='🔄replace', callback_data=f"replace::{filename}"))
     if notpath.endswith('.exe') or notpath.endswith('.py'):
         markup.row(types.InlineKeyboardButton(text='▶️run', callback_data=f"run::{filename}"))
     markup.row(types.InlineKeyboardButton(text='🔙', callback_data=f"explore::{os.path.dirname(filename)}"),
@@ -108,6 +120,19 @@ def download(call):
     bot.send_document(chat_id=call.message.chat.id, reply_markup=markup, document=data)
 
 
+@bot.callback_query_handler(func=lambda call: call.data.startswith('delete'))
+def delete(call):
+    bot.answer_callback_query(call.id)
+    filename, action = call.data.split('::')[1:]
+    if action:
+        os.remove(filename)
+    msg = explorer_func(os.path.split(filename)[0])
+    if not msg:
+        msg = explorer_func('.')
+    bot.edit_message_text(text='your files', reply_markup=msg, chat_id=call.message.chat.id,
+                          message_id=call.message.id)
+
+
 @bot.callback_query_handler(func=lambda call: call.data == 'close')
 def close(call):
     bot.delete_message(message_id=call.message.id, chat_id=call.message.chat.id)
@@ -116,6 +141,16 @@ def close(call):
 @bot.message_handler(content_types=['text', 'audio', 'photo', 'video', 'media', 'file', 'voice', 'video_note'])
 def deleter(message):
     bot.delete_message(message.chat.id, message.id)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('&'))
+def conf_action(call):
+    callback = call.data[1:]
+    markup = types.InlineKeyboardMarkup()
+    markup.row(types.InlineKeyboardButton('confirm✅', callback_data=f"{callback}::1"))
+    markup.row(types.InlineKeyboardButton("cancel❌", callback_data=f"{callback}::"))
+    bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text="confirm action:",
+                          reply_markup=markup)
 
 
 try:
--- pyRPC/util/tools.py
+++ pyRPC/util/tools.py
@@ -1,51 +1,43 @@
 import json
 import telebot
 import os
-from functools import lru_cache
 
 
-data, base = dict(), str()
+def init():
+    if not os.path.isfile('pyrpc.cfg'):
+        with open('pyrpc.cfg', mode='w') as wf:
+            wf.write('{}')
+        return {}
+    with open('pyrpc.cfg') as rf:
+        return json.load(rf)
 
 
-def init(file):
-    global data, base
-    base = file[:file.rfind('\\')]
-    with open(f'{base}/assets/config.json') as rf:
-        data = json.load(rf)
-
-
-@lru_cache
-def create_bot(token):
-    try:
-        bot = telebot.TeleBot(token)
-    except:
-        return False
-    return bot
+data = init()
 
 
 def load_bot():
     if data.get('token', False):
-        return create_bot(data['token'])
+        if not data.get('host', False):
+            print('please, start the bot')
+        return telebot.TeleBot(data['token'])
     token = input("couldn't find the bot token, create a telegram bot and send the token: ")
-    while not create_bot(token):
-        token = input("token doesn't work, send again: ")
     print('done! now, start bot')
-    with open(f'{base}/assets/config.json', mode='w') as wf:
+    with open('pyrpc.cfg', mode='w') as wf:
         data['token'] = token
         json.dump(data, wf)
-    return create_bot(token)
+    return telebot.TeleBot(token)
 
 
 def create_host(chat_id):
     if data.get('host', False):
         return
-    with open(f'{base}/assets/config.json', mode='w') as wf:
+    with open('pyrpc.cfg', mode='w') as wf:
         data['host'] = chat_id
         json.dump(data, wf)
 
 
 def reset_token():
-    with open(f'{base}/assets/config.json', mode='w') as wf:
+    with open('pyrpc.cfg', mode='w') as wf:
         data.pop('token')
         json.dump(data, wf)
 
